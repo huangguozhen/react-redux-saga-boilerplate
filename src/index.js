c488975fc1ba4c3a49048e94cbd82578223dece7
huangguozhen <huangguozhen@outlook.com>
2018-07-30 21:30:28 +0800

    使用react-router@4.0模板。

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import { createBrowserHistory  } from 'history';
+import { Router, Route, Switch  } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
+import routes from './routes';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        {routes.map((prop, key) => (
+          <Route path={prop.path} component={prop.component} key={key} />
+        ))}
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import App from '../App';
+
+const routes = [{
+  path: '/', component: App
+}];
+
+export default routes;
